326075bd511708121e366db26c25b6555f419695
MrHidalgo <Luckyhidalgo@gmail.com>
2018-09-06 06:09:43 +0300

    Create statistic section - w\ adaptive, hover.

--- src/js/_document/indexJquery.js
+++ src/js/_document/indexJquery.js
@@ -36,6 +36,61 @@ $(document).ready((ev) => {
   };
 
 
+  /**
+   *
+   * @param circumference
+   * @param value
+   */
+  const progressData = (circumference, value) => {
+    const elem = $('.progress__value');
+
+    let progress = value / 100,
+      dashoffset = circumference * (1 - progress);
+
+    elem.animate({
+      "strokeDashoffset": dashoffset
+    }, 500);
+  };
+
+  /**
+   *
+   * @param radius
+   * @returns {number}
+   */
+  const progressCircumference = (radius) => {
+    return (2 * Math.PI * radius);
+  };
+
+  /**
+   *
+   */
+  const initProgressCircle = () => {
+    const progressValue = $('.progress__value'),
+      radius = progressValue.attr("r");
+
+    progressValue.css({
+      "strokeDasharray": progressCircumference(radius)
+    });
+
+    progressData(progressCircumference(radius), 72);
+  };
+
+  const statisticBtn = () => {
+    $(".statistic__block-btn").on("click", (e) => {
+      const elem = $(e.currentTarget),
+        elemAttr = elem.data("val"),
+        statisticDataElem = $("[statistic-data-js]");
+
+      $(".statistic__block-btn").removeClass("is-active");
+      elem.addClass("is-active");
+
+      statisticDataElem.html(elemAttr + "<span>%</span>");
+
+      progressData(progressCircumference($('.progress__value').attr("r")), elemAttr);
+    });
+  };
+
+
 
   /**
    * @description Init all method
@@ -46,6 +101,8 @@ $(document).ready((ev) => {
     initObjectFitImages();
     // ...
     initSelectricJs();
+    initProgressCircle();
+    statisticBtn();
   };
   initJquery();
 });
